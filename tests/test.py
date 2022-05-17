a17315ab41deb098e2beef5bc8f3dafff9a1bb76
Pau <pausic98@gmail.com>
2022-05-17 20:39:38 +0200

    Estableciendo estructura

---
+++ tests/test.py
@@ -0,0 +1,32 @@
+import unittest
+import snake
+
+class myTests(unittest.TestCase):
+	def test1(self):
+		'''
+		board: [4, 3]
+		snake: [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
+		depth: 3
+		'''
+		self.assertEqual(number_of_available_different_paths([4,3], [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]], 3), 7)
+
+	def test2(self):
+		'''
+		board: [2, 3]
+		snake: [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]]
+		depth: 10
+		'''
+		self.assertEqual(number_of_available_different_paths([2,3], [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]], 10), 1)	
+
+	def test3(self):
+		'''
+		board: [10, 10]
+		snake: [[5,5], [5,4], [4,4], [4,5]]
+		depth: 4
+		'''
+		self.assertEqual(number_of_available_different_paths([10,10], [[5,5], [5,4], [4,4], [4,5]], 4), 81)	
+
+
+
+if __name__ == "__main__":
+	unittest.main()
