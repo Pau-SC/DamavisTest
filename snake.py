94b9c09ae80c1de0aae34dbc5a3583912e86948f
Pau <pausic98@gmail.com>
2022-05-18 13:06:38 +0200

    Quitada la opcion de lanzar por cmd

--- snake.py
+++ snake.py
@@ -1,6 +1,13 @@
 from collections import deque
+from argparse import ArgumentParser
 import sys
 
+'''
+Moves the tail to the new position of the snake, returning an updated version.
+
+Parameter snake_deque: The deque representing the snake to move
+Parameter direction: Can be either 'left', 'down', 'right' or 'up', deciding the movement.
+'''
 def move_snake(snake_deque, direction):
 	snake_deque_copy = snake_deque.copy()
 	head_position = snake_deque_copy.popleft() #Find the snake's head
@@ -19,17 +26,20 @@ def move_snake(snake_deque, direction):
 	snake_deque_copy.appendleft((x,y))
 	return snake_deque_copy
 
-
+'''
+Recursive funtion to look through all the possible movements of the snake given a board and the deque object
+of the snake specified on the main function.
+'''
 def number_of_available_different_paths_recursive(board, snake_deque, depth_remaining):
 	result = 0
-	if depth_remaining <= 0: #If depth is done, add 1
+	if depth_remaining <= 0: #If depth is done, we found a path, so we add 1
 		return 1
 	else:
 		snake_deque_copy = snake_deque.copy()
 		snake_deque_checker = snake_deque.copy()
-		snake_deque_checker.pop()
-		head_position = snake_deque_copy.popleft() #Find the snake's head
-		snake_deque_copy.clear()
+		snake_deque_checker.pop() #Snake can move where tail was, so we won't consider it an obstacle
+		head_position = snake_deque_copy.popleft() #Find the snake's head with a copy
+		snake_deque_copy.clear() #Will be repurposed for relative position within recursivity
 		x, y = head_position[0], head_position[1]
 		if x > 0: #Left
 			if snake_deque_checker.count((x-1,y)) == 0:
@@ -50,19 +60,27 @@ def number_of_available_different_paths_recursive(board, snake_deque, depth_rema
 		return result
 
 
+'''
+The main function, initializes a deque with the snake for usage and starts the recursive function.
 
+Returns the number of available movements with the given depth and board
+
+	Parameter board: Array containing 2 values, being the maximum x and y of the canvas where the snake exists.
+					 Precondition: from 1x1 to 10x10
+	Parameter snake: Array contains all the snake occupied positions in order.
+					 Precondition: At least 3 elements, and a maximum of 7.
+					 Precondition: Within board domain.
+					 Precondition: Correct coordinates.
+	Parameter depth: The amount of movements needed to be considered valid.
+					 Precondition: Will be at least 1, and a maximum of 20.
+'''
 def number_of_available_different_paths(board, snake, depth):
 	snake_deque = deque()
 	for snake_positions in snake: 
-		i, j = snake_positions[0], snake_positions[1]
 		snake_deque.append((snake_positions[0], snake_positions[1])) #Create a deque object with the snake in order
 	result = number_of_available_different_paths_recursive(board, snake_deque, depth)
 	return result
 
 
 if __name__ == "__main__":
-	boardInput = sys.argv[1]
-	snakeInput = sys.argv[2]
-	depthInput = sys.argv[3]
-	result = number_of_available_different_paths(boardInput, snakeInput, depthInput)
-	print(result)
+	print('Used as module!')
